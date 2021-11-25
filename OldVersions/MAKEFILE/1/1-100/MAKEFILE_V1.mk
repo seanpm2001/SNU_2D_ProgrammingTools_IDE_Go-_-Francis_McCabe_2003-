339e0b062a100141e0fd9baab05348a1080acc89
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-24 22:45:04 -0800

    MAKEFILE_V1.mk
    An archive of the first version of the makefile for this project.

---
+++ OldVersions/MAKEFILE/1/1-100/MAKEFILE_V1.mk
@@ -0,0 +1,6 @@
+rename /Go! (Frances McCabe, 2003) to /Go!-(Francis-McCabe,-2003) # Not valid yet
+# This file needs work
+# File info
+# File type: GNU Makefile (*.mk)
+# File version: 1 (2021, Wednesday, November 24th at 10:44 pm)
+# Line count (including blank lines and compiler line): 7
