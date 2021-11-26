7d7782cb1e6887f6623718627da259b3d1ea4f39
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-25 20:06:22 -0800

    MAKEFILE.mk (V2)
    Version 2 of the makefile for this project.

--- MAKEFILE.mk
+++ MAKEFILE.mk
@@ -1,6 +1,15 @@
+# Start of script
+# This makefile makes some minor name changes. It is not functional yet.
 rename /Go! (Frances McCabe, 2003) to /Go!-(Francis-McCabe,-2003) # Not valid yet
+# Rename the secondary copying license
+# Since I don't know how to just rename a file, I will copy it and delete the original, that is as close as I can get with GNU Make right now
+copy /COPYINGL to /COPYING
+rm -f /COPYINGL
+echo "COPYING license file has been corrected."
+# This syntax isn't valid yet, I don't know how to write in GNU Make very well
 # This file needs work
 # File info
 # File type: GNU Makefile (*.mk)
-# File version: 1 (2021, Wednesday, November 24th at 10:44 pm)
-# Line count (including blank lines and compiler line): 7
+# File version: 2 (2021, Thursday, November 25th at 8:06 pm)
+# Line count (including blank lines and compiler line): 16
+# End of script
